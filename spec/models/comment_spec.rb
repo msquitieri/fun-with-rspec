686983076a8ef0a55d2863a459ae6d754c786d43
squit <michael.squitieri@gmail.com>
2013-11-04 16:05:58 -0500

    finished comment spec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -5,12 +5,18 @@ describe Comment do
   	@comment = FactoryGirl.create(:comment)
   end
 
-  it "has a score"
+  it "has a score" do
+    @comment.score.should be_present
+  end
 
-  it "belongs to a link"
+  it "belongs to a link" do
+    link = FactoryGirl.create(:link)
+    link.comments << @comment
+
+    @comment.link.should == link
+  end
 
   it "increments the score" do
-    ## this simulates the vote-up functionality.
     @comment.vote_up
     @comment.score.should == 2
   end
