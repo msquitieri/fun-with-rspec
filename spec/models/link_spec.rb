e9cc14a355275094e221d8ab0b637273d09b531c
squit <michael.squitieri@gmail.com>
2013-11-04 16:06:48 -0500

    finished link spec

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -5,9 +5,13 @@ describe Link do
   	@link = FactoryGirl.create(:link)
   end
 
-  it "has a url"
+  it "has a url" do
+    @link.url.should be_present
+  end
 
-  it "has a score"
+  it "has a score" do
+    @link.score.should be_present
+  end
 
   it "belongs to a user" do
   	user = FactoryGirl.create(:user)
@@ -21,15 +25,21 @@ describe Link do
   		@link = FactoryGirl.create(:link_with_comment)
   	end
 
-    it "has a comment"
+    it "has a comment" do
+      @link.comments.count.should == 1
+    end
 
     it "updates its cumulative score when a comment gets a score" do
-      ## This one is tricky. You'll have to have several lines of code here. 
-      ## The idea is that a link with 2 comments, one of a score 2, the other of a score 3, 
-      ## will have a composite score of 5.
-
-      ## A hint is that you may want to leverage the after_save callback of the 
-      ## comment method to maybe update the parent link. 
+      3.times { @link.comments << FactoryGirl.create(:comment, score: 10, link:@link) }
+      
+      comment = @link.comments.first
+      4.times { comment.vote_up }
+      comment.save
+      
+      @link.save
+      @link.reload
+      
+      @link.score.should == 35
     end
   end
 end
