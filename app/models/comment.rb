f5f46e14ee699384e42e55eec586f06538378ad0
squit <michael.squitieri@gmail.com>
2013-11-04 16:05:24 -0500

    implemented vote_up and update_cumulative_score

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,13 @@
 class Comment < ActiveRecord::Base
   belongs_to :link
+
+  after_save :update_cumulative_score
+
+  def vote_up
+  	self.score += 1
+  end
+
+  def update_cumulative_score
+  	self.link.update_cumulative_score if self.link
+  end
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,14 @@
 class Link < ActiveRecord::Base
   belongs_to :user
   has_many :comments
+
+  def update_cumulative_score
+  	total = 0
+  	self.comments.each do |comment|
+  		total += comment.score if comment.score
+  	end
+
+  	self.score = total
+    self.save
+  end
 end
